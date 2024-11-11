e06efac47b002f25c60a629d5c3cf33a9f676735
༒ V O X T O ༒ <129228963+V0XTO@users.noreply.github.com>
2024-11-11 10:44:37 -0600

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False).lower() == True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ") 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 
 # Application definition
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 database_url = os.environ.get("DATABASE_URL")
-DATABASES['default'] =  dj_database_url.parse("database_url")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
